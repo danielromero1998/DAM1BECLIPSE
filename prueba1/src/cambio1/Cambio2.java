8de2012587a318bbee4718e602da251ebbe91271
daniel <email@address.com>
2021-04-19 17:04:22 +0200

    subir

--- prueba1/src/cambio1/Cambio2.java
+++ prueba1/src/cambio1/Cambio2.java
@@ -4,7 +4,9 @@ public class Cambio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("estoy cambio 2 proyecto 2");
+		System.out.println("segunda linea en cambio 2");
+		
 	}
 
 }
