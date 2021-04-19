d7551dd821bb4ab0d1de07599da75f03819fac0e
daniel <email@address.com>
2021-04-19 16:35:41 +0200

    validacion de cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
  public static void main(String[] arg) {
 	 System.out.println("hola mundo");
 	 System.out.println("otra linea");
+	 System.out.println("otra linea2");
  }
 }
