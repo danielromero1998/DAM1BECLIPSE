3e5c6de2ea8f96519b00d0b24ceb9a08561e6615
daniel <email@address.com>
2021-04-19 16:38:39 +0200

    validando tercera linea

--- prueba1/src/cambio1/cambio1.java
+++ prueba1/src/cambio1/cambio1.java
@@ -3,5 +3,7 @@ package cambio1;
 public class cambio1 {
 	 public static void main(String[] arg) {
 		 System.out.println("hola mundo");
+		 System.out.println("otra linea");
+		 System.out.println("otra linea2");
 	 }
 }
